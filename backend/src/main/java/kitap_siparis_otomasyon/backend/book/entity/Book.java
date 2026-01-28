dfc1892f3b854b435e649e2dffbfd99296492305
emirnar <emirnar2701@gmail.com>
2026-01-28 22:38:16 +0300

    Book entity created , books datas from csv imported into books table

---
+++ backend/src/main/java/kitap_siparis_otomasyon/backend/book/entity/Book.java
@@ -0,0 +1,38 @@
+package kitap_siparis_otomasyon.backend.book.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "books")
+@Getter @Setter
+public class Book {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "request_name", nullable = false)
+    private String requestName;
+
+    @Column(name = "order_name", nullable = false)
+    private String orderName;
+
+    @Column(name = "isbn", nullable = false)
+    private String isbn;
+
+    protected Book() {
+    }
+
+    public Book(String requestName, String orderName, String isbn) {
+        this.requestName = requestName;
+        this.orderName = orderName;
+        this.isbn = isbn;
+    }
+
+}
+
