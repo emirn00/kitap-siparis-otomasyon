31a5241c8bd342a148211d4e6fa89eaa915ea870
Nazli Gonca <nazligonca@Nazlis-MacBook-Air.local>
2026-02-11 14:37:44 +0300

    logout button added

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() {}
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
